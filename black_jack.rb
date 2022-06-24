df9c25bc7281e538de1c3ccc031c50d14975c031
Ryan Mullen <ryanfmullen@gmail.com>
2022-06-23 23:13:22 -0700

    fix: code and comment

--- black_jack.rb
+++ black_jack.rb
@@ -1,9 +1,18 @@
-All_cards = { 'A' => [1, 11], 'K' => 10, 'Q' => 10, 'J' => 10, 
+card_scores = { 'A' => [1, 11], 'K' => 10, 'Q' => 10, 'J' => 10, 
 '10' => 10, '9' => 9, '8' => 8, '7' => 7, '6' => 6, 
 '5' => 5, '4' => 4, '3' => 3, '2' => 2, '1' => 1 }
 # how to make it so there are 4 of each card? hashes have unique keys
 #could do 'AD' 'AS' 'AH' 'AC'
+suits = ['D', 'H', 'S', 'C']
 
+ALL_CARDS = Hash.new
+suits.each do |suit|
+    card_scores.each do |k, v|
+        ALL_CARDS[k + suit] = v
+    end
+end
+
+p ALL_CARDS
 
 
 
@@ -12,12 +21,12 @@ def game_play
     
     max_score = 21
 
-    cards_arr = All_cards.keys
+    cards_arr = ALL_CARDS.keys
     player_hand = cards_arr.sample(2)
     p player_hand
     dealer_hand = cards_arr.sample(2)
 
-    player_score = player_hand.each {|card| (all_cards[card].to_i.sum)}
+    player_score = player_hand.each {|card| (ALL_CARDS[card].sum)}
     p player_hand
     p player_score
     p dealer_hand[0]
@@ -38,12 +47,24 @@ end
 
 # on each turn/hit need to check score against 21 
 # how do you choose 11 or 1 for an ace?
-# how do you remove that card (k,v) from the hash
-# on each trun dealer score compared to 18 if bellow, dealer hits
+# how do you remove that card (k,v) from the hash -> own method
+
 # need to make messages for "you win the hand", "bust", "dealer wins the hand"
 # play again? y, n
 # error handling for when someing other than hit or stay is input
 
+#Black Jack class
+# instance vars = @deck(via generate_deck Class#method), @dealer, @player
+# instance methods = deal(will use hit intance method 
+# - give cards to dealer and player and remove them from the ALL CARDS - use .shuffle and deal first card in deck)
+# instance method = win, lose, bust, hit, play(initialize game), 
+# inst method(after every round need to check num_cards to see if > .33 * num_cards)
 
+
+
+#Dealer Class - @hit(if score <= 16), @dealer_score(attr_accessor), 
+#inst method for ace(dealer hits on soft 17)
+#Player Class - where we get user input and do most error handling, @player_score(attr_accessor)
+#inst method for ace(if hand with 11 > 21 -> ace becomes 1)
 
 game_play
